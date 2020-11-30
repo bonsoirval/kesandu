50bc0a54b75c14325d570736d84e07a0f12d8080
NJOKU OKECHUKWU VALENTINE <bonsoirval@gmail.com>
2020-11-30 14:19:23 +0100

    Blog module now showing on module list.

---
+++ admin/controller/extension/blog.php
@@ -0,0 +1,6 @@
+<?php
+class ControllerBlogBlog extends Controller{
+  public function index(){
+    
+  }
+}
---
+++ admin/language/en-gb/extension/extension/blog.php
@@ -0,0 +1,15 @@
+<?php
+// Heading
+$_['heading_title']    = 'Blog';
+
+// Text
+$_['text_success']     = 'Success: You have modified blog!';
+$_['text_list']        = 'Blog List';
+
+// Column
+$_['column_name']      = 'Blog Name';
+$_['column_status']    = 'Status';
+$_['column_action']    = 'Action';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify Blog!';
